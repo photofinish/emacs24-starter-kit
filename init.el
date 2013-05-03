501ad3c4be64c79e0ebba3cf02ec08e7e521c631
Exaos-memes <exaos@ciae.ac.cn>
2013-05-03 16:28:56 +0800

    Initialize Chinese translation

--- init.el
+++ init.el
@@ -1,29 +1,30 @@
-;;; init.el --- Where all the magic begins
+;;; init.el --- 魔法从这儿开始
 ;;
-;; Part of the Emacs Starter Kit
+;; Emacs 起步套件的一部分
 ;;
-;; This is the first thing to get loaded.
+;; 这是首先要加载的东西。
 ;;
 
-;; load Org-mode from source when the ORG_HOME environment variable is set
+;; 如果设置了环境变量 ORG_HOME, 则尝试从此位置载入 Org-mode.
 (when (getenv "ORG_HOME")
   (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
     (when (file-directory-p org-lisp-dir)
       (add-to-list 'load-path org-lisp-dir)
       (require 'org))))
 
-;; load the starter kit from the `after-init-hook' so all packages are loaded
+;; 从 `after-init-hook' 载入起步套件，然后是所有其它的包
 (add-hook 'after-init-hook
  `(lambda ()
-    ;; remember this directory
+    ;; remember this directory --  记住此目录
     (setq starter-kit-dir
           ,(file-name-directory (or load-file-name (buffer-file-name))))
     ;; only load org-mode later if we didn't load it just now
+    ;; 如果现在还没有载入 org-mode, 赶紧加载吧
     ,(unless (and (getenv "ORG_HOME")
                   (file-directory-p (expand-file-name "lisp"
                                                       (getenv "ORG_HOME"))))
        '(require 'org))
-    ;; load up the starter kit
+    ;; load up the starter kit -- 载入起步套件
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
